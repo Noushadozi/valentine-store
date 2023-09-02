a367b0da2531ba5e045e7534fbc41fedf384a466
Noushadozi <noushadozi333@gmail.com>
2023-09-02 20:44:03 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Dashboard from './components/Dashboard/Dashboard'
+import Navbar from './components/Navbar/Navbar'
+import Products from './components/Products/Products'
+
+function App() {
+
+  return (
+    <>
+      <Navbar></Navbar>
+      <Products></Products>
+      <Dashboard></Dashboard>
+    </>
+  )
+}
+
+export default App
